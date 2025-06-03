f554f8b5ce583cf9f4d54ee5cd906d4ccc7ecd90
ardakabadayi <kabadayi_arda@hotmail.com>
2025-06-03 19:22:43 +0300

    use openai api for image generation

--- backend/app/crew/tools/image_generation_tool.py
+++ backend/app/crew/tools/image_generation_tool.py
@@ -1,8 +1,8 @@
 from crewai.tools.base_tool import BaseTool
-import torch
-from diffusers import AmusedPipeline
+import base64
 import uuid
-from pydantic import PrivateAttr
+from app.crew.constants import client
+import re
 
 class ImageGenerationTool(BaseTool):
     """
@@ -10,28 +10,32 @@ class ImageGenerationTool(BaseTool):
     """
     name: str = "Image Generation Tool"
     description: str = "A tool that generates multiple images from a given fairytale. It returns the image paths."
-
-    _pipe: AmusedPipeline = PrivateAttr()
-
-    def __init__(self):
-        super().__init__()
-        self._pipe = AmusedPipeline.from_pretrained(
-            "amused/amused-512", variant="fp16", torch_dtype=torch.float16
-        )
-    
+ 
     def _run(self, fairytale_text: str):
         """
         Generates images from a given fairytale
         """
         image_paths = []
-        pages = str(fairytale_text).split("---------")
+        pages = re.findall(r"<page>(.*?)</page>", fairytale_text, re.DOTALL)
+        pages = [page.strip() for page in pages]
         
         for page in pages:
             if len(page.strip()) > 0:
-                image = self._pipe(page.strip(), generator=torch.manual_seed(0)).images[0]
+                result = client.images.generate(
+                    model="gpt-image-1",
+                    prompt=page.strip(),
+                    size="1024x1024"
+                )
+                
                 id = str(uuid.uuid4())
                 image_path = f"generated/{id}.png"
-                image.save(image_path)
+                
+                image_base64 = result.data[0].b64_json
+                image_bytes = base64.b64decode(image_base64)
+
+                with open(image_path, "wb") as f:
+                    f.write(image_bytes)
+
                 image_paths.append(image_path)
 
         return image_paths
